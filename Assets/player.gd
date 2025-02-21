2532964704913d0272cf235574d45ea1ada71371
EPham42747 <ethancpham@gmail.com>
2025-02-21 18:33:11 -0500

    Added player look

--- Assets/player.gd
+++ Assets/player.gd
@@ -4,6 +4,22 @@ extends CharacterBody3D
 const SPEED = 3.0
 const JUMP_VELOCITY = 4.5
 
+@onready var camera := $Camera3D
+@onready var body := $"."
+const TURN_SPEED_FACTOR = 0.001
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED and event is InputEventMouseMotion:
+		camera.rotate_x(-event.relative.y * TURN_SPEED_FACTOR)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		body.rotate_y(-event.relative.x * TURN_SPEED_FACTOR)
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
